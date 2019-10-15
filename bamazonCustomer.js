d574427088c12f6c6d89ed4463a75b78d36e4af2
zuprocj <jmdcorpuz@gmail.com>
2019-10-15 15:56:47 -0700

    customer js added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,19 +11,53 @@ var connection = mysql.createConnection({
 });
 
 function displayAll(){
-    connection.query('SELECT * FROM prodcuts', function(err, res) {
-        if (err) {console.log(error)};
+    connection.query('SELECT * FROM prodcuts', function(error, response) {
+        if (error) throw error;
         var displayTable = new table({
             head: ['Item ID', 'Product Name', 'Department', 'Price', 'Stock'],
             colWidths: [10,30,18,10,14]
         });
         for (i=0; i < response.length; i++) {
             displayTable.push(
-                [response[i].item_id, response.[i].prodcut_name, response[i]department_name,
+                [response[i].item_id, response.[i].prodcut_name, response[i].department_name,
                 response[i].price, response[i].stock_quantity]
             );
         }
         console.log(displayTable.toString());
         inquirePurchase();
-    })
+    });
+};
+function inquirePurchase(){
+    inquirePurchase.prompt([
+        {
+            name: 'ID',
+            type: 'input',
+            message: 'What is the item number of the item you would like purchase?'
+        }, {
+            name: 'Quantity',
+            type: 'input',
+            message: 'How many would you like to buy?'
+        },
+    ]).then(function(answers){
+        var quantityWanted = answers.Quantity;
+        var idWanted = answers.ID;
+        purchaseFromDatabasae(idWanted, quantityWanted);
+    });
+};
+function purchaseFromDatabasae(ID, quantityNeeded) {
+    connection.query('SELECT * FROM prodcuts WHERE item_id = ' + ID, function(error, response) {
+        if (error) throw error;
+        if (quantityNeeded <= response[0].stock_quantity) {
+            var totalCost = response.[0].price * quantityNeeded;
+            console.log('We have what you need. Order coming up!');
+            console.log('Your total cost for ' + quantityNeeded + ' ' + response[0].prodcut_name +
+             ' is ' + totalCost + '. Thank you for your purchase.')
+            connection.query('UPDATE prodcuts SET stock_quantity = stock_quantity - ' + quantityNeeded +
+            ' WHERE item_id = ' + ID);
+        } else {
+            console.log("Our apologies. We don't have enough " + response[0].prodcut_name + " to fulfill your order.");
+        };
+        displayAll();
+    });
 }
+displayAll();
