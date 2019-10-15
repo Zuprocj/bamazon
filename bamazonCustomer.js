f90a32bdd3936fe61c13116a483da9b91a5e5827
zuprocj <jmdcorpuz@gmail.com>
2019-10-15 15:32:06 -0700

    bamazoncustomer created. table created

---
+++ bamazonCustomer.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var table = require('cli-table');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: "productsDB"
+});
+
+function displayAll(){
+    connection.query('SELECT * FROM prodcuts', function(err, res) {
+        if (err) {console.log(error)};
+        var displayTable = new table({
+            head: ['Item ID', 'Product Name', 'Department', 'Price', 'Stock'],
+            colWidths: [10,30,18,10,14]
+        });
+        for (i=0; i < response.length; i++) {
+            displayTable.push(
+                [response[i].item_id, response.[i].prodcut_name, response[i]department_name,
+                response[i].price, response[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        inquirePurchase();
+    })
+}
