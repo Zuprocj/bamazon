19cafc0b39c409b4458633d994aa38ea017a697d
zuprocj <jmdcorpuz@gmail.com>
2019-10-16 21:04:18 -0700

    deleted supervisor table

--- bamazon.sql
+++ bamazon.sql
@@ -13,9 +13,3 @@ CREATE TABLE products (
     PRIMARY KEY (item_id)
 );
 
-CREATE TABLE departments (
-    department_id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR (45) NOT NULL,
-    over_head_costs DECIMAL (10,2),
-    PRIMARY KEY (department_id)
-);
